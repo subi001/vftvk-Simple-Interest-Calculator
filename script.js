bcd035261611e5ee5aa44a9ab956766c26c63a61
subi001 <85447880+subi001@users.noreply.github.com>
2021-06-12 16:39:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-function compute()
+
+function updateRate() 
 {
-    p = document.getElementById("principal").value;
+   
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("result").innerHTML="";
     
 }
+
+
+function  ComputeInterest(){
+
+   var principal=document.getElementById("principal").value;  
+   if(principal<=0){
+              alert("Enter a positive number");
+              document.getElementById("principal").focus();
+    } 
+    else{
+               var rate=document.getElementById("rate").value;
+               var years=document.getElementById("years").value;
+               console.log(years);
+               var amount=principal * years * rate/100;
+               console.log(principal * rate);
+               var year = new Date().getFullYear()+parseInt(years);
+               document.getElementById("result").innerHTML="<br>If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    }
+
+}
+
+function clearResult()
+{
+       document.getElementById("result").innerHTML="";
+}
+
+ 
+
+   
+
+
+
+
         
